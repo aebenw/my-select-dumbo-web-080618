9e3d90f7719af1ddba6996bb6a79693826d54458
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 23:49:01 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
  arr = []
  i = 0 
  while i < collection.length 
-  arr << yield(collection[i])
+  if block_given? == true 
+    arr << yield(collection[i])
+    end
   i += 1 
   end 
   arr
